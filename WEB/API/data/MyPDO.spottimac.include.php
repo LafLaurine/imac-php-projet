923578e26668af9c204c4ecfdf160c7e2509d3d8
Margaus Séguy <margaux.seguy66@gmail.com>
2019-04-12 11:59:50 +0200

    accueil
    texte accueil

--- WEB/API/data/MyPDO.spottimac.include.php
+++ WEB/API/data/MyPDO.spottimac.include.php
@@ -1,4 +1,4 @@
 <?php
 require_once 'MyPDO.class.php';
-MyPDO::setConfiguration('mysql:host=localhost;dbname=spottimac;charset=utf8', 'root', '');
+MyPDO::setConfiguration('mysql:host=localhost;dbname=spottimac;charset=utf8', 'root', 'root');
 ?>
