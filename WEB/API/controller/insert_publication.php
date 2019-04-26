163f3aadda42efbee996be0845fc4df1fd98fbba
Victorine <victorine.maurel@hotmail.fr>
2019-04-26 18:37:15 +0200

    Insertion img BDD

--- WEB/API/controller/insert_publication.php
+++ WEB/API/controller/insert_publication.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 // check HTTP method
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 
-if ($method !== 'get') {
+if ($method !== 'post') {
 	http_response_code(405);
 	echo json_encode(array('message' => 'This method is not allowed.'));
 	exit();
@@ -16,30 +16,32 @@ include_once "../data/MyPDO.spottimac.include.php";
 
 // response status
 http_response_code(200);
-$valeurs = ['titre'=>'', 'datepub'=>'', 'topic'=>'', 'content' =>''];
-if(!empty($_GET)){
-	if(isset($_GET['titre']) && !empty($_GET['titre'])){
-		$valeurs['titre'] = $_GET['titre'];			
+
+//insertion dans la table publication 
+$champsPublication = ['titre'=>'', 'datepub'=>'', 'topic'=>'', 'content' =>''];
+if(!empty($_POST)){
+	if(isset($_POST['titre']) && !empty($_POST['titre'])){
+		$champsPublication['titre'] = $_POST['titre'];			
 	}
-	if(isset($_GET['topic']) && !empty($_GET['topic'])){
+	if(isset($_POST['topic']) && !empty($_POST['topic'])){
 		$req_id = MyPDO::getInstance()->prepare(<<<SQL
 	SELECT *
 	FROM topic
-	WHERE nom_topic = $_GET['topic']	
+	WHERE nom_topic = $_POST['topic']	
 SQL
 );
 		$req_id->execute();
 		while (($row = $req_id->fetch()) !== false) {
-			$valeurs['topic'] = $row['id_topic'];
+			$champsPublication['topic'] = $row['id_topic'];
 		}	
 	}
-	if(isset($_GET['content']) && !empty($_GET['content'])){
-		$valeurs['content'] = $_GET['content'];			
+	if(isset($_POST['content']) && !empty($_POST['content'])){
+		$champsPublication['content'] = $_POST['content'];			
 	}
 }
 
 $date = date('Y-m-d');
-$valeurs['datepub'] = $date ;
+$champsPublication['datepub'] = $date ;
 
 $stmt = MyPDO::getInstance()->prepare(<<<SQL
 	INSERT INTO publication(titre_publication, date_publication, id_topic, content)
@@ -47,15 +49,38 @@ $stmt = MyPDO::getInstance()->prepare(<<<SQL
 SQL
 );
 
-$insertion = $stmt->execute($valeurs);
+$insertionPublication = $stmt->execute($champsPublication);
 
-if ($insertion){
-	$message = 'Operation enregistree avec succes';
-}
-else{
-	$message = 'Operation non enregistree ';
-}
+$idPub = MyPDO::lastInsertId();
 
-echo json_encode($message);
+// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+if (isset($_FILES['file']) AND $_FILES['file']['error'] == 0){
+	if ($_FILES['file']['size'] <= 1000000){
+		$infosfichier = pathinfo($_FILES['file']['name']);
+		$extension_fichier = $infosfichier['extension'];
+		//testons si le fichier est une image 
+		$extensions_image = array('jpg', 'jpeg', 'gif', 'png');
+		if (in_array($extension_upload, $extensions_image)){
+			$destination = "../../STOCKAGE/IMAGE";
+			$champsImg = ['idPub' =>'','nom'=> '', 'taille'=>'', 'type'=>'', 'description' =>'', 'blob_image'=>''];
+			$nom = $_FILES['file']['name'];
+			$chemin = $destination.'/'.$nom;
+			$champsImg['idPub']= $idPub; 
+			$champsImg['nom']= $nom; 
+			$champsImg['taille']=$_FILES['file']['size'];
+			$champsImg['type']=$infosfichier['extension'];
+			$champsImg['blob_image']= $chemin;
+
+			move_uploaded_file($_FILES['file']['tmp_name'],$chemin);
+
+			$insertImg = MyPDO::getInstance()->prepare(<<<SQL
+			INSERT INTO publication(id_publication, nom_image, taille_image, type_image, desc_image,blob_image)
+			VALUES (:idPub, :nom, :taille, :type, :description, :blob_image)
+SQL
+);
+			$insertImg->execute($champsImg);
+		}
+	}
+}
 exit();
 ?>
