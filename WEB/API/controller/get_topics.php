66441ac166c02989bd01c5c0c22b8abef5790ea3
Line Rathonie <line.jpn25@gmail.com>
2019-05-05 18:58:01 +0200

    get topic depending on cat

--- WEB/API/controller/get_topics.php
+++ WEB/API/controller/get_topics.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 // check HTTP method
 $method = strtolower($_SERVER['REQUEST_METHOD']);
+$stmt;
 
 if ($method !== 'get') {
 	http_response_code(405);
@@ -13,34 +14,59 @@ if ($method !== 'get') {
 
 // include data
 include_once "../data/MyPDO.spottimac.include.php";
+$topics;
+if(!empty($_GET['publicategorie'])) {
+	$topics= tri_by_cat($_GET['publicategorie']);
+} else {
+	$topics= allTopic();
+}
+function tri_by_cat($id)
+{
+	// response status
+	http_response_code(200);
 
-// response status
-http_response_code(200);
-
-$stmt = MyPDO::getInstance()->prepare(<<<SQL
-	SELECT *
-	FROM topic
-	ORDER BY nom_topic
+	$stmt = MyPDO::getInstance()->prepare(<<<SQL
+		SELECT nom_topic
+		FROM topic
+		LEFT JOIN categories
+		ON topic.id_categorie = categories.id_categorie
+		WHERE categories.id_categorie = :publicategorie
+		ORDER BY nom_topic
 SQL
-);
+	);
 
-$stmt->execute();
-$topic = [];
-
-while(($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
-	array_push($topic,$row); 
+	$stmt->execute(array(':publicategorie'=>$id));
+	return $stmt;
 }
 
-if(empty($topic)) {
-	echo json_encode(array("error" => "Missing topic"));
-	http_response_code(422);
+function allTopic() {
+	$stmt = MyPDO::getInstance()->prepare(<<<SQL
+		SELECT nom_topic
+		FROM topic
+		ORDER BY nom_topic
+SQL
+	);
+	$stmt->execute();
+	return $stmt;
 }
-echo json_encode($topic,JSON_UNESCAPED_UNICODE);
+
+	$topic = [];
+
+	while(($row = $topics->fetch(PDO::FETCH_ASSOC))) {
+		array_push($topic,$row); 
+	}
+
+	if(empty($topic)) {
+		echo json_encode(array("error" => "Missing topic"));
+		http_response_code(422);
+	}
+
+	echo json_encode($topic,JSON_UNESCAPED_UNICODE);
 
 //allow us to get topic from categories (need to put it under categories)
 
-/*
-$topic_cat = [];
+
+/*$topic_cat = [];
 $stmt_topic_cat = MyPDO::getInstance()->prepare(<<<SQL
 	SELECT nom_topic, nom_categorie 
 	FROM topic,categories 
