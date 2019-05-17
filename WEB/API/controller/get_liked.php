2734e0963f1957c73a3197065212eca6215d3392
Victorine <victorine.maurel@hotmail.fr>
2019-05-17 15:53:01 +0200

    modif get liked

--- WEB/API/controller/get_liked.php
+++ WEB/API/controller/get_liked.php
@@ -1,10 +1,10 @@
 <?php
 // headers
+session_start();
 header("Content-Type: application/json; charset=UTF-8");
 
 // check HTTP method
 $method = strtolower($_SERVER['REQUEST_METHOD']);
-
 if ($method !== 'get') {
 	http_response_code(405);
 	echo json_encode(array('message' => 'This method is not allowed.'));
@@ -17,16 +17,22 @@ include_once "../data/MyPDO.spottimac.include.php";
 // response status
 http_response_code(200);
 
-
+//on récupère l'id de l'user
+//pour pouvoir récupèrer les publications que cette user a liké
+$id_user = $_SESSION['id_user'];
 $stmt = MyPDO::getInstance()->prepare(<<<SQL
 	SELECT *
-	FROM user_liked;
+	FROM user_liked
+	WHERE id_user = :id_user;
 SQL
 );
 
+$stmt->bindParam(':id_user',$id_user);
 $stmt->execute();
 $user_liked = [];
 
+//on met le résultat de la requête dans un tableau 
+//et on la renvoie
 while (($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
 	array_push($user_liked,$row); 
 }
@@ -34,5 +40,7 @@ if(empty($user_liked)) {
 	echo json_encode(array("error" => "Missing publications"));
 	http_response_code(422);
 }
+
+
 echo json_encode($user_liked,JSON_UNESCAPED_UNICODE);
 exit();
--- WEB/JS/generate_objects.js
+++ WEB/JS/generate_objects.js
@@ -153,6 +153,25 @@ document.ready( () => {
 		})
 		.catch(error => { console.log(error) });
 
+		fetch("./API/controller/get_liked.php")
+		.then( response => response.json() )
+		.then( data => {
+			var select = document.getElementsByClassName("reac");
+			console.log(select);
+			for (var i = 0; i < select.length; i++) {
+				var id_publi = select[i].getAttribute("data-id_publication");
+				console.log(id_publi);
+				for (var j = 0; j<data.length; j++){
+				 	if(id_publi == data[j].id_publication){
+				 		if (data[j].liked == "1"){
+				 			select[i].setAttribute("src", "./SRC/heart_pink.png");
+				 		}
+				 	}
+				 }
+		}
+
+		}).catch(error => { console.log(error) });
+
 		/*document.getElementById("heart-id1").onclick = event => {
 			let heart = document.getElementById("heart-id1");
 			let id_publication = heart.getAttribute("data-id_publication");
@@ -183,26 +202,7 @@ document.ready( () => {
 
 });
 
-document.ready( () => {
-	fetch("./API/controller/get_liked.php")
-	.then( response => response.json() )
-	.then( data => {
-		var select = document.getElementsByClassName("reac");
-		console.log(select);
-		for (var i = 0; i < select.length; i++) {
-			var id_publi = select[i].getAttribute("data-id_publication");
-			console.log(id_publi);
-			for (var j = 0; j<data.length; j++){
-			 	if(id_publi == data[j].id_publication){
-			 		if (data[j].liked == "1"){
-			 			select[i].setAttribute("src", "./SRC/heart_pink.png");
-			 		}
-			 	}
-			 }
-		}
 
-	}).catch(error => { console.log(error) });
-});
 
 document.getElementById("tri_default").onclick = event => {
 	fetch("./API/controller/get_publications.php")
@@ -513,7 +513,7 @@ function chngimg() {
 			console.log("Erreur");
 		}
 	}
-		request.open("POST", "http://localhost/PHP/PROJET_NEW/imac-php-projet/WEB/API/controller/add_like.php",true);
+		request.open("POST", "./API/controller/add_like.php",true);
 		request.send(body);
 		console.log(liked);
 	}
@@ -542,7 +542,7 @@ function chngimg() {
 			console.log("Erreur");
 		}
 	}
-		request.open("POST", "http://localhost/PHP/PROJET_NEW/imac-php-projet/WEB/API/controller/remove_like.php",true);
+		request.open("POST", "./API/controller/remove_like.php",true);
 		request.send(body);
 	}	
 }
